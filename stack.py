7546d57bac2f9eeebf800b4d0e6970e3f896ce9f
Dirac <daronmitchel@gmail.com>
2025-07-15 12:39:29 -0500

    Paso 1 y 2

--- stack.py
+++ stack.py
@@ -2,31 +2,23 @@ from typing import Any
 
 
 class Stack:
-    """Implementa una estructura de datos de Pila"""
 
-    def __init__(self) -> None:
-        """Constructor"""
-        self.items = []
+    def _init__(self):
+        self.stack: list[Any] = []
+        self.size = 0
 
     def push(self, data: Any) -> None:
-        """Coloca un elemento en la pila."""
-        self.items.append(data)
+        self.stack.insert(0, data)
+        self.size += 1
 
     def pop(self) -> Any:
-        """Elimina un elemento de la pila y lo devuelve."""
-        return self.items.pop()
+        if self.size > 1:
+            self.stack.pop(0)
+            self.size -= 1
 
     def peek(self) -> Any:
-        """
-        Devuelve el elemento en la parte superior de la pila
-        sin eliminarlo.
-        """
-        return self.items[-1]
+        data = self.stack[0]
+        return data
 
     def is_empty(self) -> bool:
-        """
-        Devuelve True si la pila está vacía, de lo contrario
-        devuelve False.
-        """
-        return len(self.items) == 0
-
+        return self.size == 0
